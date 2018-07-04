d8017eca1b38f8b34a8e3789ebca113e9e69d317
Fangyu Zou <37023753+fangyuzou@users.noreply.github.com>
2018-07-04 13:44:06 -0400

    Update Calculator.java

--- Shunting Yard Algorithm/Calculator.java
+++ Shunting Yard Algorithm/Calculator.java
@@ -20,18 +20,26 @@ public class Calculator {
         int j = 0;
         while (j < infix.length()) {
             char token = infix.charAt(j);
+            // if the token is an operator, check whether the peek of the ops stack is a '(' or an operator with higher 
+            // precedence or an operator with the same precedence and left associative. If so, pop out the peek and add
+            // to the postfix expression until the above is not satisfied. Then push the current operator into the stack.
             if (isOperator(token)) {
                 while (!ops.isEmpty() && ops.peek() != '(' && (precedence(token) < precedence(ops.peek()) || 
                             precedence(token) == precedence(ops.peek()) && associativity(token) == LEFT))
                      postfix.add(String.valueOf(ops.pop()));
                 ops.push(token);
             }
+            // if the token is left parenthesis, push into the stack.
             else if (token == '(')  ops.push(token);
+            // if the token is right parenthesis, pop out the peek and add it to the postfix until the peek is a left 
+            // parenthesis, then pop it out without adding to the postfix. If not such left parenthesis, it means the 
+            // infix is invalid, we throw out an exception.
             else if (token == ')') {
                 while (!ops.isEmpty() && ops.peek() != '(')  postfix.add(String.valueOf(ops.pop()));
                 if (ops.isEmpty()) throw new IllegalArgumentException("Invalid infix expression!");
                 ops.pop();
             }
+            // if the token is a digit, we parse out the whole digit string (skipping over the white spaces).
             else if (Character.isDigit(token)) {
                 StringBuilder operand = new StringBuilder();
                 while (j < infix.length() && !isOperator(infix.charAt(j)) && infix.charAt(j) != '(' 
@@ -42,16 +50,20 @@ public class Calculator {
                 postfix.add(operand.toString());
                 j--;
             }
+            // finally, if the token is a white space, we simply skip it.
             j++;
         }
+        while (!ops.isEmpty())  postfix.add(String.valueOf(ops.pop()));
         return postfix;
     }
     
     private boolean isOperator(char token) {
+        // return whether the token is an valid operator (+,-,*,/,^).
         return token == '+' || token == '-' || token == '*' || token == '/' || token == '^';
     }
     
     private int precedence(char token) {
+        // return the precedence of an operator, if not a valid operator, throw an IllegalArgumentExcepeion.
         if (token == '+' || token == '-')  return 1;
         else if (token == '*' || token == '/')  return 2;
         else if (token == '^')  return 3;
@@ -59,18 +71,24 @@ public class Calculator {
     }
     
     private boolean associativity(char token) {
+        // return whether an operator is left associative or right associative, if not a valid operator,
+        // we throw out an IllegalArgumentException.
         if (token == '+' || token == '-' || token == '*' || token == '/')  return LEFT;
         else if (token == '^')  return RIGHT;
         else throw new IllegalArgumentException("Invalid operator!");
     }
     
     public int evaluate(List<String> postfix) {
+        // Calculate the value of a valid postfix expression.
         if (postfix == null || postfix.isEmpty())  return 0;
      
         Stack<Integer> operand = new Stack<>();
         while (!postfix.isEmpty()) {
             String token = postfix.remove(0);
+            // if the token is operand, push into stack.
             if (Character.isDigit(token.charAt(0)))  operand.push(Integer.parseInt(token));
+            // if the token is an operator, pop out the two operands from the stack and do calculation under the 
+            // operator, then push the result back into stack.
             else operand.push(calculate(operand.pop(), operand.pop(), s.charAt(0)));
         }
         return operand.pop();
@@ -86,7 +104,8 @@ public class Calculator {
             default: throw new IllegalArgumentException("Illegal operator!");
         }
     }
- 
+   
+    // test
     public static void main(String[] args) {
         String infix = "2+1 0/&(2*$3-1)+,,(3..-2)*2@@^2^2";
         Calculator cal = new Calculator();
