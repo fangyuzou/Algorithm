e7ceb38e7a6e3c5e088ea614db9f19881cf2cfdc
Fangyu Zou <37023753+fangyuzou@users.noreply.github.com>
2018-10-12 17:32:11 -0400

    Update SPT.java

--- Dijkstra/SPT.java
+++ Dijkstra/SPT.java
@@ -1,44 +1,73 @@
 import java.util.HashMap;
+import java.util.List;
 import java.util.ArrayList;
 
 public class SPT {
   private int source;
-  private final HashMap<Integer, int[]> spt;
-  private final HashMap<Integer, ArrayList<int[]>> graph;
+  private final HashMap<Integer, Integer> distTo;
+  private final HashMap<Integer, int[]> edgeTo;
   
   public SPT(int[][] edges, int source) {
     this.source = source;
+    distTo = new HashMap<>();
+    edgeTo = new HashMap<>();
     
     // build graph
+    HashMap<Integer, List<int[]>> graph = new HashMap<>();
     for (int[] edge : edges) {
-      graph.computeIfAbsent(edge[0], K -> new ArrayList<int[]>()).add(edge);
+      graph.computeIfAbsent(edge[0], K -> new ArrayList<int[]>()).add(new int[]{edge[0], edge[1], edge[2]});
       graph.computeIfAbsent(edge[1], K -> new ArrayList<int[]>()).add(new int[]{edge[1], edge[0], edge[2]});
     }
     
-    spt = new HashMap<Integer, int[]>();
-    PriorityQueue<int[]> pq = new PriorityQueue<>();
-    
-    // initialize pq
-    pq.offer(source, 0);
-    for (int v : graph.keySet()) if (v != source) {
-      pq.offer(v, Integer.MAX_VALUE);
+    // initialize distTo
+    for (Integer v : graph.keySet()) {
+      distTo.put(v, Integer.MAX_VALUE);
     }
+    distTo.put(source, 0);
     
-    int prev = source;
+    PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> (a[1] - b[1]));
+    pq.offer(new int[]{source, 0});
+
     while (!pq.isEmpty()) {
-      int[] w = pq.poll();
-      if (spt.containsKey(w[0])) {
+      int[] cur = pq.poll();
+      if (distTo.containsKey(cur[0])) {
         continue;
       }
-      spt.put(new int[]{w[0], w[1], prev});
-      prev = w[0];
-      for (int[] edge : graph.get(w[0])) {
-        pq.offer(new int[]{edge[1], edge[2] + spt.get(w[0])[1]);
+      distTo.put(cur[0], cur[1]);
+      for (int[] edge : graph.get(cur[0])) {
+        int w = edge[1];
+        int wight = edge[2];
+        if (cur[1] + weight < distTo.get(w)) {
+          edgeTo.put(w, edge);
+          pq.offer(new int[]{w, cur[1] + weight});
+        }                   
       }
     }
   }
   
-  public int distance(int target) {
-    return spt.get(target).[1];
+  public int distTo(int v) {
+    return distTo.get(v);
+  }
+    
+  public boolean hasPathTo(int v) {
+    return edgeTo.containsKey(v);
+  }
+  
+  public Iterable<int[]> pathTo(int v) {
+    if (!hasPathTo(v)) {
+      return null;
+    }
+    Stack<int[]> path = new Stack<int[]>();
+    for (int[] e = edgeTo.get(v); e != null; e = edgeTo.get(e[0])) {
+      path.push(e);
+    }
+    return path;
+  }
+  
+  public static void main(String[] args) {
+    int[][] edges = {{0, 1, 8}, {0, 4, 4}, {1, 3, 2}, {1, 2, 9}, {2, 4, 3}, {2, 4, 6}, {3, 4, 5}, {5, 6, 10}};
+    SPT t = new SPT(edges, 0);
+    for (int i = 0; i < 7; i++)
+      System.out.println(t.distTo(i));
   }
 }
